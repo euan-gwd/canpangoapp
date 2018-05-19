e15e1c78275bbb1dadb65219bb433ec97fa20dbc
euan.gwd <euan@amwic.co.za>
2018-05-19 11:18:22 +0200

    Add Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  componentDidMount = () => {
+    axios.get(`http://apichallenge.canpango.com/`).then(res => {
+      console.log(res);
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Canpango Beer App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
