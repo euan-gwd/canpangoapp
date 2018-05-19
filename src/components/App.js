b8f71f9b16758ff2713145d9cda496072f071217
euan.gwd <euan@amwic.co.za>
2018-05-19 20:48:47 +0200

    Add Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import CategoryMenu from "./CategoryMenu";
 // import Beers from "./Beers";
 import logo from "./logo.png";
+import { Header, Image, Segment } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <div>
-            <img src={logo} alt="logo" />
-            <h1>BreweryDB</h1>
-          </div>
-        </header>
+        <Segment as="header" basic inverted>
+          <Image src={logo} alt="logo" centered />
+          <Header as="h1" textAlign="center">
+            <Header.Content>BreweryDB</Header.Content>
+          </Header>
+        </Segment>
         <CategoryMenu />
         <main>{/* <Beers /> */}</main>
       </div>
