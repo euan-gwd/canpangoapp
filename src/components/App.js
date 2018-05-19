fea3b7e85fd0efaa4c8ae8fe431e083d823391d5
euan.gwd <euan@amwic.co.za>
2018-05-19 21:44:53 +0200

    Finished Menu Design

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import CategoryMenu from "./CategoryMenu";
 // import Beers from "./Beers";
 import logo from "./logo.png";
@@ -7,8 +7,8 @@ import { Header, Image, Segment } from "semantic-ui-react";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Segment as="header" inverted>
+      <Fragment>
+        <Segment as="header" basic inverted>
           <Image src={logo} alt="logo" centered inverted />
           <Header as="h1" textAlign="center">
             BreweryDB
@@ -16,7 +16,7 @@ class App extends Component {
         </Segment>
         <CategoryMenu />
         <main>{/* <Beers /> */}</main>
-      </div>
+      </Fragment>
     );
   }
 }
