1b090705467e501f9d3e5959efe1b1f171e154a5
euan.gwd <euan@amwic.co.za>
2018-05-19 20:23:44 +0200

    reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "semantic-ui-css/semantic.min.css";
 import "bulma/css/bulma.min.css";
 import App from "./components/App";
 
