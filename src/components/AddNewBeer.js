f624df2360efbafdd4f01d7ce4c51ceed39aab34
euan.gwd <euan@amwic.co.za>
2018-05-20 16:36:36 +0200

    Let's test it

--- src/components/AddNewBeer.js
+++ src/components/AddNewBeer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Segment, Form } from "semantic-ui-react";
+// import axios from "axios";
 
 class AddNewBeer extends Component {
   state = {
@@ -52,7 +53,51 @@ class AddNewBeer extends Component {
       category,
       created_on
     };
-    console.log(newBeer);
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(newBeer),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(`/categories/${category}/`, options)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return Promise.reject({
+            status: res.status,
+            statusText: res.statusText
+          });
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log("Error, with message:", err.statusText));
+
+    // axios({
+    //   method: "post",
+    //   url: `/categories/${category}/`,
+    //   body: newBeer
+    // }).catch(function(error) {
+    //   if (error.response) {
+    //     // The request was made and the server responded with a status code
+    //     // that falls out of the range of 2xx
+    //     console.log(error.response.data);
+    //     console.log(error.response.status);
+    //     console.log(error.response.headers);
+    //   } else if (error.request) {
+    //     // The request was made but no response was received
+    //     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    //     // http.ClientRequest in node.js
+    //     console.log(error.request);
+    //   } else {
+    //     // Something happened in setting up the request that triggered an Error
+    //     console.log("Error", error.message);
+    //   }
+    //   console.log(error.config);
+    // });
 
     // this.setState({
     //   name: "",
